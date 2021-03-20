4819a96e597748d628347d5336c90ce6302c57ae
Rama Balla <ramakrishna.balla@gmail.com>
2021-03-21 10:39:20 +1100

    Add files via upload

---
+++ Me_In_a_Nutshell.py
@@ -0,0 +1,9 @@
+print("Enter 'Rama's Workign Status - either 'True or False' here : " )
+rama_is_working = input()
+if (rama_is_working == "True"):
+	print("\n \n ...... Enjoys the work and challenges ...... \n \n")
+elif (rama_is_working == "False"):	
+	print("\n \n ...... Learning GCP or Practicising more on to Cloud and Cloud Security ...... ")
+	print(" ...... Spends time with his family and Enjoys Mentoring his son  ...... ")
+	print(" ...... Loving the Technical chats with his son now a days :)  ...... \n \n")
+	
